2bb088f7fefc90552508bdf783b8723ddcee7597
José Alejandro Álvarez Mendoza <alvarez.alejandro3@gmail.com>
2014-09-13 16:56:11 -0430

    First Commit

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 if(!global.hasOwnProperty('db')){
 
 	var mongoose = require('mongoose');
+	var mongo = require('mongodb');
+	var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
 	// var dbName = 'yjiWy5va';
 	var dbName	= 'app29545456';
 	var username = 'hoheim';
@@ -11,9 +13,18 @@ if(!global.hasOwnProperty('db')){
 
 	// mongoose.connect('mongodb://'+username+':'+password+'@proximus.modulusmongo.net:'+port+'/'+dbName);
 
-	mongoose.connect('mongodb://'+username+':'+password+'@kahana.mongohq.com:'+port+'/'+dbName);
+	// mongoose.connect('mongodb://'+username+':'+password+'@kahana.mongohq.com:'+port+'/'+dbName);
 
+	mongo.Db.connect(mongoUri, function (err, db) { 
+		db.collection('mydocs', function(er, collection) {
+			collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+
+			});
+		});
+	});
+	
 	global.db = {
+		mongo: mongo,
 		mongoose: mongoose,
 		User: require('./User')(mongoose),
 		Dojo: require('./Dojo')(mongoose),
